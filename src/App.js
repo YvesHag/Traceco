f028b1fe9da5bbb3b8321de96932a7d1a256305c
Yves <yves@hyvemind.be>
2020-07-27 22:49:55 +0200

    two buttons register and say goodbay

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Label, Input} from 'reactstrap';
 
-function App() {
+
+const App = (props)=> {
+
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container min-vh-100">
+
+      <div className="row">
+        <div></div>
+        <div className="col-12 mt-5 d-flex justify-content-center">
+        <Button size="lg" onClick={toggle}>Check In</Button></div>
+        <div className="col-12 mt-5 d-flex justify-content-center"><Button  size="lg">Say GoodBye</Button></div>
+      </div>
+
+
+      <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Covid-19 Tracer</ModalHeader>
+        <ModalBody>
+          <FormGroup>
+            <Input type="text" name="Naam" id="Naam" placeholder="Naam" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="nationalNumber" hidden>National Number</Label>
+            <Input type="number" name="nationalNuùmber" id="National Number" placeholder="Rijksregisternummer" />
+          </FormGroup>
+          <FormGroup>
+          <Input type="text" name="email" id="Email" placeholder="Email" />
+          </FormGroup>
+          <FormGroup>
+          <Input type="number" name="GSM-Nummer" id="GSM-Nummer" placeholder="GSM-Nummer" />
+          </FormGroup>
+
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>Submit</Button>{' '}
+          <Button color="secondary" onClick={toggle}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+
     </div>
   );
 }
